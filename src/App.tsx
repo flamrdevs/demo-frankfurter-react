8f5bcdb2a39b7c71153f37a19399633a4b1560be
flamrdevs <flamrdevs@gmail.com>
2024-05-17 02:35:51 +0700

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Title } from "@mantine/core";
+
+function App() {
+  return (
+    <>
+      <main
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100dvw",
+          height: "100dvh",
+        }}
+      >
+        <Title order={2}>demo-frankfurter-react</Title>
+      </main>
+    </>
+  );
+}
+
+export default App;
